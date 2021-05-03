f4e2abaeffbdd522f2da206b9d48b05c26927372
btjam <btjam@10.0.0.146>
2021-05-03 14:40:21 -0700

    First commit for hw1

---
+++ cst438Hw1/src/main/java/cst438/Hello.java
@@ -0,0 +1,46 @@
+package cst438;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class Hello
+{
+
+   @Autowired
+   RatingRepository ratingRepository;
+
+   @GetMapping("/movies/new")
+   public String createMovieRating(Model model)
+   {
+      MovieRating ratings = new MovieRating();
+      model.addAttribute("rating", ratings);
+      return "rating_form";
+   }
+
+   @GetMapping("/movies")
+   public String getAllRatings(Model model)
+   {
+      Iterable<MovieRating> ratings = ratingRepository.findAll();
+      model.addAttribute("ratings", ratings);
+      return "rating_list";
+   }
+
+   @PostMapping("/movies")
+   public String processMovieRatingForm(@Valid MovieRating rating, BindingResult result, Model model)
+   {
+      if(result.hasErrors())
+      {
+         return "rating_form";
+      }
+      ratingRepository.save(rating);
+      return "rating_show";
+   }
+
+}
---
+++ cst438Hw1/src/main/java/cst438/RatingRepository.java
@@ -0,0 +1,8 @@
+package cst438;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RatingRepository extends CrudRepository<MovieRating, Long>
+{
+
+}
